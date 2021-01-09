a79d451dd25c8a7314bc477ce79aa48b98b3d03b
janhvi <janhvi1009@gmail.com>
2021-01-09 19:23:42 +0530

    entry

--- widgets/entry.py
+++ widgets/entry.py
@@ -1,1 +1,22 @@
-fro
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import messagebox
+
+window = Tk()
+window.title("Button")
+window.geometry("500x400")
+
+
+label1 = Label(window, text="Enter Your Name")
+label1.pack()
+
+name_entry = Entry(window)
+name_entry.pack()
+
+def submit_button_click():
+    messagebox.showinfo("Message", "Your Name: " + name_entry.get()) 
+
+submit_button = Button(window, text = "Submit", command = submit_button_click)
+submit_button.pack()
+
+window.mainloop()
--- widgets/window.py
+++ widgets/window.py
@@ -9,6 +9,8 @@ window.geometry("800x500")
 #label1 = Label(window, text="Welcome to Python GUI")
 #label1.pack()
 
+
+# grid widget
 label00 = Label(window, text= "First Row First Column")
 label00.grid(row = 0, column = 0)
 label01 = Label(window, text= "First Row Second Column")
@@ -17,11 +19,6 @@ label10 = Label(window, text= "Second Row First Column")
 label10.grid(row = 1, column = 0)
 label11 = Label(window, text= "Second Row Second Column")
 label11.grid(row = 1, column = 1)
-
-
-
-
-
 
 
 
